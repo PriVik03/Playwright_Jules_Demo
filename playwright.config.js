054957840d332170b9ad4ceb5e9fc1e47b633f36
Priyanka More <priyanka.more9038@gmail.com>
2025-02-20 18:09:44 +0530

    Modification to tests

--- playwright.config.js
+++ playwright.config.js
@@ -23,14 +23,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter:[["allure-playwright",{ outputFolder:'allure-results'}],
-            ['html'],
-            ['list'],
-            ['line'],
-            ['dot'],
-            ['junit', { outputFile:'results.xml'}],
-            ['json', { outputFile:'results.json'}]
-          ],
+  reporter:'html',
+  // reporter:[["allure-playwright",{ outputFolder:'allure-results'}],
+  //           ['html'],
+  //           ['list'],
+  //           ['line'],
+  //           ['dot'],
+  //           ['junit', { outputFile:'results.xml'}],
+  //           ['json', { outputFile:'results.json'}]
+  //         ],
+  timeout: 50000, // Set default timeout to 60 seconds
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
